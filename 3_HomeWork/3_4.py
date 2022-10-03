22569eec21be65d82b7cee9cb5b8b16548c564eb
Nikolay Pisarev <pisarev82@yandex.ru>
2022-10-03 09:58:59 +0300

    Все еще ДЗ 3

--- 3_HomeWork/3_4.py
+++ 3_HomeWork/3_4.py
@@ -3,18 +3,8 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
-x = 150
+x = 2
 y = str(bin(x))
-print(y[2:])
-# while x != 0:
-# x = 45//2
-# print(x)
-n=x
-m=n
-p=1
-d1=0
-while m>0:
-    d1=d1+m%2*p
-    p=p*10
-    m=m//2
-print(d1)
+print("Я строка: ", y[2:])
+print("Я число: ", int(y[2:]))
+
