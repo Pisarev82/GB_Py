18263750925dd3776280a24f3ee0e99372a6ea91
Nikolay Pisarev <pisarev82@yandex.ru>
2022-10-07 12:15:54 +0300

    Рушение ДЗ 4

--- 4_HomeWork/4_4.py
+++ 4_HomeWork/4_4.py
@@ -14,13 +14,8 @@ print(a, b, c)
 result = ''
 if b == 0: result = f"{a}*x**{k} + {c}*x**{k-2} = 0"
 if b == 0 and c == 0: result =  f"{a}*x**{k} = 0"
-# if c == 0: result = f"{a}*x**{k} + {b}*x**{k-1} = 0 "
 else: result =  f"{a}*x**{k} + {b}*x**{k-1} + {c}*x**{k-2} = 0 "
 
 with open("file444.txt", "w") as file44:
     file44.write(result)
 
-
-
-# b = int(input('Введите число заначений:'))
-# a = [random.randint(0,10) for _ in range(b)]
