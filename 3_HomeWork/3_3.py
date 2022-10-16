800cc5bf022914268c545135895932a035e896dd
Nikolay Pisarev <pisarev82@yandex.ru>
2022-10-16 23:25:19 +0300

    Текущая работа

--- 3_HomeWork/3_3.py
+++ 3_HomeWork/3_3.py
@@ -3,9 +3,11 @@
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 list1 = [1.1, 1.2, 3.1, 5.0, 10.01]
-list3 = []
-for i in range(len(list1)):
-    if round((list1[i] - int(list1[i])), 2) != 0:
-        list3.append(round((list1[i] - int(list1[i])), 2))
+# list3 = []
+# for i in range(len(list1)):
+#     if round((list1[i] - int(list1[i])), 2) != 0:
+#         list3.append(round((list1[i] - int(list1[i])), 2))
+# print(max(list3) -  min(list3))
 
-print(max(list3) -  min(list3))
+list2 = [round((list1[i] - int(list1[i])), 2) for i in range(len(list1)) if round((list1[i] - int(list1[i])), 2) != 0]
+print(max(list2) -  min(list2))
